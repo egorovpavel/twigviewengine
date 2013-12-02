a984fa91beea6c6f1d71489814108e85e6e570a5
Pavel Egorov <pavel@egorov.ru>
2013-12-02 22:57:44 +0000

    Dummy implementation

---
+++ twigviewengine/TwigViewEngine.php
@@ -0,0 +1,13 @@
+<?php
+namespace twigviewengine {
+
+    use egorovpavel\compot\lib\IViewEngine;
+
+    class TwigViewEngine implements IViewEngine{
+
+        public function render($template, $data)
+        {
+            // TODO: Implement render() method.
+        }
+    }
+}
