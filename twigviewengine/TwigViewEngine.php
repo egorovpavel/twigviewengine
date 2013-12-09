71a00daf9f1558dee3520bbbb4dea76fb8ecc398
Pavel Egorov <pavel@egorov.ru>
2013-12-09 20:46:15 +0000

    Added urlFor extension

--- twigviewengine/TwigViewEngine.php
+++ twigviewengine/TwigViewEngine.php
@@ -1,6 +1,7 @@
 <?php
 namespace twigviewengine {
 
+    use compot\HttpApplication;
     use compot\IViewEngine;
 
     class TwigViewEngine implements IViewEngine{
@@ -15,12 +16,25 @@ namespace twigviewengine {
          */
         protected $twig;
 
-        public function __construct($viewPath){
+        /**
+         * @var HttpApplication
+         */
+        protected $app;
+
+        public function __construct($viewPath, HttpApplication $app){
+            $this->app = $app;
             $this->loader = new \Twig_Loader_Filesystem([strtolower($viewPath)]);
             $this->twig = new \Twig_Environment($this->loader, [
                 'cache' => strtolower($viewPath) . '/cache',
                 'debug' => true
             ]);
+
+
+            $this->twig->addFunction(new \Twig_SimpleFunction('urlFor', function ($routeName, $action = null, $controller = null, $arguments = []) use($app){
+                $route = $app->getRoute($routeName);
+                return $route->generateFor($controller, $action, $arguments);
+            }));
+
         }
 
         public function render($template, $data)
