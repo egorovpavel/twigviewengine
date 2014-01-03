10c93dd98cfd67c28853262433f539adf6331a22
Pavel Egorov <pavel@egorov.ru>
2014-01-03 20:39:16 +0000

    Code style fixes

--- twigviewengine/TwigViewEngine.php
+++ twigviewengine/TwigViewEngine.php
@@ -4,7 +4,8 @@ namespace twigviewengine {
     use compot\HttpApplication;
     use compot\IViewEngine;
 
-    class TwigViewEngine implements IViewEngine{
+    class TwigViewEngine implements IViewEngine
+    {
 
         /**
          * @var \Twig_Loader_String
@@ -21,7 +22,8 @@ namespace twigviewengine {
          */
         protected $app;
 
-        public function __construct($viewPath, HttpApplication $app){
+        public function __construct($viewPath, HttpApplication $app)
+        {
             $this->app = $app;
             $this->loader = new \Twig_Loader_Filesystem([strtolower($viewPath)]);
             $this->twig = new \Twig_Environment($this->loader, [
@@ -30,42 +32,42 @@ namespace twigviewengine {
             ]);
 
 
-            $this->twig->addFunction(new \Twig_SimpleFunction('urlFor', function ($routeName, $action = null, $controller = null, $arguments = []) use($app){
+            $this->twig->addFunction(new \Twig_SimpleFunction('urlFor', function ($routeName, $action = null, $controller = null, $arguments = []) use ($app) {
                 $route = $app->getRoute($routeName);
                 return $route->generateFor($controller, $action, $arguments);
             }));
 
-            $this->twig->addFunction(new \Twig_SimpleFunction('validationMessageFor', function ($context, $property) use($app){
-                if(isset($context['_errors'])){
-                    foreach($context['_errors'] as $violation){
-                        if($violation->getPropertyPath() == $property){
+            $this->twig->addFunction(new \Twig_SimpleFunction('validationMessageFor', function ($context, $property) use ($app) {
+                if (isset($context['_errors'])) {
+                    foreach ($context['_errors'] as $violation) {
+                        if ($violation->getPropertyPath() == $property) {
                             return $violation->getMessage();
                         }
                     }
-                }else{
+                } else {
                     return null;
                 }
                 return "";
-            },['needs_context' => true]));
+            }, ['needs_context' => true]));
 
-            $this->twig->addFunction(new \Twig_SimpleFunction('isValid', function ($context, $property, $onTrue, $onFalse) use($app){
-                if(isset($context['_errors'])){
-                    foreach($context['_errors'] as $violation){
-                        if($violation->getPropertyPath() == $property){
+            $this->twig->addFunction(new \Twig_SimpleFunction('isValid', function ($context, $property, $onTrue, $onFalse) use ($app) {
+                if (isset($context['_errors'])) {
+                    foreach ($context['_errors'] as $violation) {
+                        if ($violation->getPropertyPath() == $property) {
                             return $onFalse;
                         }
                     }
-                }else{
+                } else {
                     return null;
                 }
                 return $onTrue;
-            },['needs_context' => true]));
+            }, ['needs_context' => true]));
 
         }
 
         public function render($template, $data)
         {
-            return $this->twig->render(strtolower($template). '.twig' , $data);
+            return $this->twig->render(strtolower($template) . '.twig', $data);
         }
     }
 }
